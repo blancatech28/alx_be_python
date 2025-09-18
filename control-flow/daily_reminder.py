f6971a3dace61195abf4caa8ccca0aa58eea1102
Blancatech <Ebenezergyimah52@gmail.com>
2025-09-18 21:06:13 +0000

    daily remainder script

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,33 @@
+while True:
+    task = input("Describe your task: ")
+    task_priority = input("What is the task priority? (high, medium, low):  ")
+    time_bound = input("Is it time-bound? yes or no: ")
+
+    match task_priority:
+
+        case "high":
+            if time_bound == "yes":
+                print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+            else:
+                print(f"Note: '{task}' is a high priority task. Consider completing it when you have soon.")
+
+        case "medium":
+            if time_bound == "yes":
+                print(f"Reminder: '{task}' is a medium priority task that requires moderate attention anytime!")
+            else:
+                print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
+
+        case "low":
+            if time_bound == "yes":
+                print(f"Reminder: '{task}' is a low priority task that requires low attention anytime!")
+            else:
+                print(f"Note: '{task}' is a low priority task. Take your time.")
+
+
+        case _:
+            print("Invalid input. Try again")
+            continue
+    break
+    
+
+print("Done")
