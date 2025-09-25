d3ee8371f298916ef0ab5c041e89016f8a66abb3
Blancatech <Ebenezergyimah52@gmail.com>
2025-09-25 20:38:46 +0000

    fix: temperature conversion formula

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -10,15 +10,16 @@ temp_unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper
 
 def convert_to_celsius(fahrenheit):
     if temp_unit == "F":
-        temp_celsius = round(fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR, 1)
+        temp_celsius = round((fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR, 1)
         print(f"{fahrenheit}°F is {temp_celsius}°C")
         return temp_celsius
 
 def convert_to_fahrenheit(celsius):
     if temp_unit == "C":
-        temp_fahrenheit = round(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR, 1)
+        temp_fahrenheit = round(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32, 1)
         print(f"{celsius}°C is {temp_fahrenheit}°F")
         return temp_fahrenheit
+
 
 
 if temp_unit == "F":
