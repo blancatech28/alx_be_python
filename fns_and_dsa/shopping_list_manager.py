da59c5259c109c50b3f7f24abf183719c9678c90
Blancatech <Ebenezergyimah52@gmail.com>
2025-09-25 17:47:28 +0000

    shopping list project

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,46 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            add_item = input("Please add your item: ").strip().lower()
+            shopping_list.append(add_item)
+            print(f"{add_item} added to list")
+
+
+
+        elif choice == 2:
+            remove_item = input("Please enter your item to remove: ").strip().lower()
+            if remove_item in shopping_list:
+                shopping_list.remove(remove_item)
+                print(f"{remove_item} removed from list")
+            else:
+                print(f"{remove_item} is not in your shopping list")
+
+
+        elif choice == 3:
+            if not shopping_list:
+                print("You have not added item yet")
+
+            else:
+                print(f"This is your shopping list: {shopping_list}")
+
+
+        elif choice == 4:
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again")
+
+
+
+main()
