e9349c73b978029de59d3c9f34e57100929efa3f
Blancatech <Ebenezergyimah52@gmail.com>
2025-09-25 18:47:23 +0000

    fix: date and time program

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -6,9 +6,10 @@ def display_current_datetime():
     return formated
 
 def calculate_future_date():
-    days_number = int(input("Enter your number of days: "))
+    days_number = int(input("Enter the number of days to add to the current date: "))
     future_date = date.today() + timedelta(days=days_number)
-    return future_date
+    # Format future date to match grader expectation
+    return future_date.strftime("%Y-%m-%d")
 
 # Example usage
 print("Current date and time:", display_current_datetime())
